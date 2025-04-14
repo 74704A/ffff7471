76228e820ac5a1ac18d7bb7372cd7c8a137d29d8
74704A <231552@astanait.edu.kz>
2025-04-14 10:11:09 +0500

    correct img

--- lib/screens/main_page.dart
+++ lib/screens/main_page.dart
@@ -40,7 +40,7 @@ class MainPage extends StatelessWidget {
               children: [
                 _sectionTitle('Популярное'),
                 SizedBox(
-                  height: 220, // высота карточки
+                  height: 250, // высота карточки
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     itemCount: bookList.length,
@@ -49,8 +49,10 @@ class MainPage extends StatelessWidget {
                       return Padding(
                         padding: const EdgeInsets.only(left: 16.0, right: 8.0),
                         child: SizedBox(
-                          width: 160, // ширина карточки
-                          child: BookCard(book: book),
+                          width: 140, // ширина карточки
+                          child: BookCard(book: book,
+                              imageHeight: 160,
+                            ),
                         ),
                       );
                     },
@@ -80,13 +82,14 @@ class MainPage extends StatelessWidget {
               delegate: SliverChildBuilderDelegate(
                     (context, index) {
                   final book = bookList[index];
-                  return BookCard(book: book);
+                  return BookCard(book: book,
+                    imageHeight: 220,);
                 },
                 childCount: bookList.length,
               ),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                childAspectRatio: 0.7,
+                childAspectRatio: 0.62,
                 mainAxisSpacing: 10,
                 crossAxisSpacing: 10,
               ),
--- lib/widgets/book_card.dart
+++ lib/widgets/book_card.dart
@@ -4,49 +4,75 @@ import 'package:flutter/material.dart';
 
 class BookCard extends StatelessWidget {
   final Map<String, dynamic> book;
+  final double imageHeight;
 
-  const BookCard({required this.book});
+  const BookCard({
+    super.key,
+    required this.book,
+    this.imageHeight = 160,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: Colors.grey[200],
-            image: DecorationImage(
-              image: NetworkImage(book['image']),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 3),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Обложка книги
+          ClipRRect(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+            child: Image.network(
+              book['image'],
+              height: imageHeight,
+              width: double.infinity,
               fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return Container(
+                  height: imageHeight,
+                  color: Colors.grey[300],
+                  alignment: Alignment.center,
+                  child: Icon(Icons.broken_image, size: 40),
+                );
+              },
             ),
           ),
-        ),
-        // Иконка избранного
-        Positioned(
-          right: 8,
-          top: 8,
-          child: Icon(Icons.favorite_border, color: Colors.white),
-        ),
-        // Детали книги
-        Positioned(
-          bottom: 0,
-          child: Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(6),
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.6),
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(12)),
-            ),
+
+          // Название и цена
+          Padding(
+            padding: const EdgeInsets.all(12.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(book['title'], style: TextStyle(color: Colors.white)),
-                Text(book['price'], style: TextStyle(color: Colors.white)),
+                Text(
+                  book['title'],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 6),
+                Text(
+                  book['price'],
+                  style: TextStyle(color: Colors.grey[600]),
+                ),
               ],
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
